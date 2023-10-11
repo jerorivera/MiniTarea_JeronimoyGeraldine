4c054fa30c15d8ef40f210462c5ec236a6f3768f
JERONIMO <jeronimoriveramonroy@gmail.com>
2023-10-11 08:36:17 -0500

    Jero

--- src/MiniTarea.java
+++ src/MiniTarea.java
@@ -9,5 +9,14 @@ public class MiniTarea {
         System.out.print ("Ingresar el Segundo Número: ");
         int numero2= input.nextlnt();
 
+        int suma = numero1 + numero2;
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es " + suma);
+
+        int resta = numero1 - numero2;
+        System.out.println("La resta de " + numero1 + " y " + numero2 + " es " + resta);
+
+        int multiplicacion = numero1 * numero2;
+        System.out.println("La multiplicación de " + numero1 + " y " + numero2 + " es " + multiplicacion);
+
     }
 }
