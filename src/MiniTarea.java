f3727ed985a214f2f3af7253601032ac8cd6df26
JERONIMO <jeronimoriveramonroy@gmail.com>
2023-10-11 08:30:49 -0500

    Changes by Geraldine

--- src/MiniTarea.java
+++ src/MiniTarea.java
@@ -4,10 +4,10 @@ public class MiniTarea {
     public static void main(String[] args){
     scanner input = new Scanner(System.in);
     System.out.print ("Ingresar el Primer Número: ");
-    Int numero1= input.nextlnt();
+    int numero1= input.nextlnt();
 
         System.out.print ("Ingresar el Segundo Número: ");
-        Int numero2= input.nextlnt();
+        int numero2= input.nextlnt();
 
     }
 }
